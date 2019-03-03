3ddb50380387e173f6c06a4cf35a0c947023bddc
Rajat Gupta <rajatgupta21@hotmail.com>
2019-03-03 13:50:59 -0500

    Added joystick controls and some of the switching controls

--- Assets/Scripts/CameraMovement2.cs
+++ Assets/Scripts/CameraMovement2.cs
@@ -12,6 +12,13 @@ public class CameraMovement2 : MonoBehaviour {
 	public Text starboardText;
     public GameObject node;         // camera rig node
     public TriggerControl triggerControl;
+    public float moveSpeed = 2f;
+    public float rotateSpeedPitch = 2f;
+    public float rotateSpeedRoll = 2f;
+    public Button portButton;
+    public Button starboardButton;
+    public Text cannonActivatedText;
+    public Text cannonAssignedText;
 
     [HideInInspector]
     public bool p1Enabled = false;
@@ -39,9 +46,19 @@ public class CameraMovement2 : MonoBehaviour {
     private bool started = false;
     private string floorStatus = "down";
     private float subHighCutoff = 15.0f;
+    public bool cannonEnabled = false;
+    private float pitch;
+    private float roll;
+    private bool cannonAssigned = false;
+    private bool portCannons = false;
+    private bool starboardCannons = false;
+    private bool cannonEnableFirst = false;
 
     // Use this for initialization
     void Start () {
+        portButton.interactable = false;
+        starboardButton.interactable = false;
+        cannonActivatedText.text += " " + cannonEnabled;
         InvokeRepeating("Position", 0.1f, 0.1f);
         Invoke("SetupFloor", 3.0f);
         // just test how many joystick are seen
@@ -76,6 +93,28 @@ public class CameraMovement2 : MonoBehaviour {
         }
     }
 
+    public void StarboardButton()
+    {
+        // this is called when the Starboard button on the puppet screen is clicked by the tour guide. The guest on the starboard side will man the cannons
+        cannonAssigned = true;
+        starboardCannons = true;
+        portCannons = false;
+        starboardButton.interactable = false;
+        portButton.interactable = false;
+        cannonAssignedText.text += " Starboard";
+    }
+
+    public void PortButton()
+    {
+        // this is called when the Port button on the puppet screen is clicked by the tour guide. The guest on the port side will man the cannons
+        cannonAssigned = true;
+        starboardCannons = false;
+        portCannons = true;
+        starboardButton.interactable = false;
+        portButton.interactable = false;
+        cannonAssignedText.text += " Port";
+    }
+
     // Update is called once per frame
     void Update () {
         
@@ -83,6 +122,8 @@ public class CameraMovement2 : MonoBehaviour {
         if (!started) {
             starboardText.text = "Starboard Controller - ";
             portText.text = "Port Controller - ";
+            //starboardText.text += "LH: " + Input.GetAxis("P1-HorizontalLeft") + "; LV: " + Input.GetAxis("P1-VerticalLeft") + "; RH: " + Input.GetAxis("P1-HorizontalRight") + "; RV: " + Input.GetAxis("P1-VerticalRight");
+            //portText.text += "LH: " + Input.GetAxis("P2-HorizontalLeft") + "; LV: " + Input.GetAxis("P2-VerticalLeft") + "; RH: " + Input.GetAxis("P2-HorizontalRight") + "; RV: " + Input.GetAxis("P2-VerticalRight");
         } 
 
         // check for joystick or keyboard input => store the input  AND test for tutorial advancement
@@ -102,6 +143,31 @@ public class CameraMovement2 : MonoBehaviour {
                 ///triggerControl.TriggerEvent("Trigger #2-3 - experience begins");
             ///}
         }
+        if (!cannonEnabled)
+        {
+            pitch = Input.GetAxis("P1-VerticalLeft");
+            roll = Input.GetAxis("P2-HorizontalRight");
+        }
+        if (cannonEnabled)
+        {
+            if (!cannonEnableFirst)
+            {
+                cannonActivatedText.text = "Cannon Activated - " + cannonEnabled;
+                portButton.interactable = true;
+                starboardButton.interactable = true;
+                cannonEnableFirst = true;
+            }
+            if (starboardCannons)
+            {
+                pitch = Input.GetAxis("P2-VerticalLeft");
+                roll = Input.GetAxis("P2-HorizontalRight");
+            }
+            else if (portCannons)
+            {
+                pitch = Input.GetAxis("P1-VerticalLeft");
+                roll = Input.GetAxis("P1-HorizontalRight");
+            }
+        }
 
         /*if (canRise == true) {
             rise = Input.GetButton("P1-A"); // rcc - changed to bool since Input returns bool
@@ -119,11 +185,12 @@ public class CameraMovement2 : MonoBehaviour {
                 ///triggerControl.TriggerEvent("Trigger #2-5-6 - experience begins");
             ///}
         }*/
-
+        
         // update the position of the node
         node.transform.Translate(Vector3.forward * forward * Time.deltaTime*6.0f);
         ///node.transform.Translate(Vector3.up * up * Time.deltaTime * 15.0f);     // rcc - add in rise and fall
-        node.transform.Rotate(Vector3.up, turn * Time.deltaTime * 10.0f);
+        node.transform.Rotate(Vector3.up, roll * Time.deltaTime * 10.0f * rotateSpeedRoll);
+        node.transform.Rotate(Vector3.right, pitch * Time.deltaTime * 10.0f * rotateSpeedPitch);
         // rcc - cap the sub vertical height
         ///if (node.transform.position.y > subHighCutoff) { node.transform.position = new Vector3(node.transform.position.x, subHighCutoff, node.transform.position.z); }
 
@@ -131,7 +198,13 @@ public class CameraMovement2 : MonoBehaviour {
 
     void Position()
     {
-        forward = Math.Abs(Input.GetAxis("Vertical"));
+        if (Input.GetKey(KeyCode.Space))
+        {
+            forward = moveSpeed;
+        }
+        else {
+            forward = 0.0f;
+        }
         /*
         // how far off are the sticks from each other
         float dif = left - right;
@@ -226,14 +299,15 @@ public class CameraMovement2 : MonoBehaviour {
 			// do something
 
 			break;
-		case "P1-LeftStick": 
-			// do something
+		case "P1-LeftStick":
+                // do something
+                if (!started) starboardText.text += " Left Stick pressed";
 
-			break;
-		case "P1-RightStick": 
-			// do something
-
-			break;
+                break;
+		case "P1-RightStick":
+                // do something
+                if (!started) starboardText.text += " Right Stick pressed";
+                break;
 
 		case "P2-A": 
 			// help the tour guide to know this is the P1 stick
@@ -269,14 +343,14 @@ public class CameraMovement2 : MonoBehaviour {
 			// do something
 
 			break;
-		case "P2-LeftStick": 
-			// do something
-
-			break;
-		case "P2-RightStick": 
-			// do something
-
-			break;
+		case "P2-LeftStick":
+                // do something
+                if (!started) starboardText.text += " Left Stick Pressed";
+                break;
+		case "P2-RightStick":
+                // do something
+                if (!started) starboardText.text += " Right Stick pressed";
+                break;
 		
 		}
 	}
